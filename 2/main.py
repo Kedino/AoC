a85bc652382c28fee8d90c20fcc73bb86957fa4f
Kedino <kedino@gmail.com>
2025-12-12 18:49:09 +0100

    found sum of repeated numbers

--- 2/main.py
+++ 2/main.py
@@ -3,8 +3,27 @@
 def main():
     filepath = 'input.txt'
     ranges = get_ranges(filepath)
-    print(ranges) # Debug print
+    # print(ranges) # Debug print
+    double_numbers = []
+    for section in ranges:
+        start, end = map(int, section.split("-"))
+        for i in range(start, end):
+            if is_double(i):
+                double_numbers.append(i)
+    total = sum(double_numbers)
+    print(total)
+
+def is_double(number):
+    num_str = str(number)
+    length = len(num_str)
+
+    if length % 2 != 0:
+        return False
     
+    half = length // 2
+    first_half = num_str[:half]
+    second_half = num_str[half:]
+    return first_half == second_half
 
 def get_ranges(filepath):
     with open(filepath) as file:
