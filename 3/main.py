05de456b56092648e29150a9c59ba1447b0efa4f
Kedino <kedino@gmail.com>
2025-12-15 20:41:40 +0100

    Started messing with logic

--- 3/main.py
+++ 3/main.py
@@ -3,7 +3,20 @@
 def main():
     filepath = 'input.txt'
     banks = get_banks(filepath)
-    print(banks) #debug print
+    #print(banks) #debug print
+
+    results = []
+    for bank in banks:
+        bank_str = str(bank)
+        length = len(bank_str)
+        n = 9
+        highest = []
+        while n > 0:
+            if n in bank_str:
+                if n == bank_str[length]:
+                    break
+
+        n -= 1
 
 def get_banks(filepath):
     with open(filepath) as file:
